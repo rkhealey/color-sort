e860358b45779b59fb0d470827105ad27dd30482
rkhealey <hello@rkhealey.com>
2017-05-20 15:59:01 +1000

    Button component

---
+++ src/components/button.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import { color } from '../utils/constants';
+
+const Button = ({ onPress, text }) =>
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.text}>{text}</Text>
+  </TouchableOpacity>;
+
+const styles = StyleSheet.create({
+  button: {
+    borderColor: color.BLACK,
+    borderRadius: 4,
+    borderWidth: 2,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+  text: {
+    color: color.BLACK,
+    fontSize: 24,
+  },
+});
+
+Button.PropTypes = {
+  onPress: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
